86a9c64c955bcf7175d21fdc58e2fa72b61a4a30
Charles Liang <lczc1988@msn.cn>
2017-09-15 16:31:45 +0800

    first creation

--- Netcoregithub/Startup.cs
+++ Netcoregithub/Startup.cs
@@ -27,7 +27,7 @@ namespace Netcoregithub
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
+            // Add framework services. 
             services.AddMvc();
         }
 
